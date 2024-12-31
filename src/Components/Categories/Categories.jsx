3b96ceba428eca2929f8cc0c3a2741cdd1e852d8
Aakash <aakashsuresh455@gmail.com>
2024-12-31 15:16:48 +0530

    Update : updated the categories page for the responsive style

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -3,17 +3,17 @@ import './Categories.css';
 
 export const Categories = () => {
   return (
-    <div className={`container-fluid categories-container`}>
+    <div className="container"> {/* Added mt-70vh class */}
       <div className="text-center categories-header">
         <h1>We Deal In</h1>
       </div>
       <div className="categories-content">
-        <div className="category-item"> 
+        <div className="category-item">
           <div className="category-card">
             <img
               className="category-image"
               src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"
-              alt="product"
+              alt="Wood Processing"
             />
             <h2 className="category-title">Wood Pressing machines</h2>
           </div>
@@ -23,7 +23,7 @@ export const Categories = () => {
             <img
               className="category-image"
               src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"
-              alt="product"
+              alt="Stone Processing"
             />
             <h2 className="category-title">Stone Processing Machines</h2>
           </div>
@@ -33,7 +33,7 @@ export const Categories = () => {
             <img
               className="category-image"
               src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"
-              alt="product"
+              alt="Laser Machines"
             />
             <h2 className="category-title">Laser Machines</h2>
           </div>
