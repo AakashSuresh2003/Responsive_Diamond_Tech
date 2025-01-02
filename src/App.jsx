d1399c4a56fd449a3fdb36788c548c9cab7f57ad
Aakash <aakashsuresh455@gmail.com>
2025-01-03 01:47:52 +0530

    modified responsioveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import { HomePage } from '../src/Components/HomePage/HomePage';
 import AboutsUSDetailed from './Components/AboutsUSDetailed/AboutsUSDetailed';
